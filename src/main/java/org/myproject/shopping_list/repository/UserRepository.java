e01d12edc1b41aeef5ded1e6918247aadb08e957
Anastasia Okuonghae <aokuonghae@gmail.com>
2020-05-27 19:04:02 -0500

    fixed broken fragment html

--- src/main/java/org/myproject/shopping_list/repository/UserRepository.java
+++ src/main/java/org/myproject/shopping_list/repository/UserRepository.java
@@ -5,12 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD:src/main/java/org/myproject/shopping_list/models/data/UserRepository.java
-@Repository("userRepository")
-public interface UserRepository extends JpaRepository<User, Integer> {
-=======
 public interface UserRepository extends CrudRepository<User, Integer> {
->>>>>>> authentication:src/main/java/org/myproject/shopping_list/repository/UserRepository.java
     User findByUsername(String username);
     User findByEmail(String email);
 }
